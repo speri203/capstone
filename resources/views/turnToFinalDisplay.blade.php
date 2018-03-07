0ccb6a01b1e9c80935882231315c0feee636dd46
Sai Peri <sai.peri@ndus.edu>
2018-03-07 00:28:07 -0600

    Moving Cesium container to laravel blade content

--- resources/views/turnToFinalDisplay.blade.php
+++ resources/views/turnToFinalDisplay.blade.php
@@ -2,6 +2,24 @@
 
 @section('content')
 
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale-1, user-scalable=no">
+	<title>Turn to Final</title>
+	<!--Cesium libraries -->
+	<script src="ThirdParty/Cesium/Cesium.js"></script>
+	<style>@import url(../../public/Cesium/ThirdParty/Cesium/Widgets/widgets.css);</style>
+	<link rel="stylesheet" href="../../public/Cesium/index.css">
+</head>
+<body>
+	<div id="cesiumContainer"></div>
+	<script src="../../public/Cesium/Source/App.js"></script>
+</body>
+</html>
+
+<!--
 <script>
     <?php
         echo "var obj = $data;";
@@ -13,5 +31,6 @@
     });
     
 </script>
+-->
 
 @endsection
